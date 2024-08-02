db22b65e717efce7f6865e9c6baf68c0ccdc7ac9
iMinloha <2821163570@qq.com>
2024-08-03 01:33:03 +0800

    malloc异常，需修改tlsf算法

--- Application/src/MessageQueue.c
+++ Application/src/MessageQueue.c
@@ -8,7 +8,7 @@
  * @brief 系统消息队列
  * @note 消息队列位于RAM中
  * */
-static MessageQueue_t SystemMessageQueue;
+MessageQueue_t SystemMessageQueue;
 
 /**
  * @brief 初始化消息队列
@@ -17,13 +17,13 @@ static MessageQueue_t SystemMessageQueue;
 void initMessageQueue(){
     SystemMessageQueue = (MessageQueue_t) tlsf_malloc(sizeof(struct MessageQueue));
     SystemMessageQueue->head = (MessageNode_t) tlsf_malloc(sizeof(MessageNode_t));
-
+    SystemMessageQueue->tail = (MessageNode_t) tlsf_malloc(sizeof(MessageNode_t));
     SystemMessageQueue->mutex = (Mutex_t) tlsf_malloc(sizeof(struct _mutex));
     mutex_init(SystemMessageQueue->mutex);
 
     SystemMessageQueue->head->subnode = NULL;
     SystemMessageQueue->length = 0;
-    SystemMessageQueue->head->next = SystemMessageQueue->tail;
+    SystemMessageQueue->head->next = NULL;
 //    u_print("%d\n", SystemMessageQueue->head->next == SystemMessageQueue->tail);
 }
 
@@ -36,27 +36,28 @@ void initMessageQueue(){
  * @param messageType 消息类型
  * */
 void publishMessage(char *messageName, void *message, int messageSize, MessageType messageType){
-    // u_print("%d\n", SystemMessageQueue->head->next == SystemMessageQueue->tail);
     mutex_lock(SystemMessageQueue->mutex);
-    u_print("%d\n", SystemMessageQueue->mutex->lockflag);
     // 创建消息节点
-    MessageNode_t messageNode = tlsf_malloc(sizeof(struct MessageNode));
+    MessageNode_t messageNode = (MessageNode_t) tlsf_malloc(sizeof(struct MessageNode));
     // 初始化消息节点, 消息类型, 消息名称, 消息, 消息大小
     messageNode->messageType = messageType;
     messageNode->message = tlsf_malloc(messageSize);
-    messageNode->messageName = tlsf_malloc(strlen(messageName) + 1);    // +1为了存放字符串结束符
+    messageNode->messageName = (char*) tlsf_malloc(strlen(messageName) + 1);    // +1为了存放字符串结束符
     // 拷贝消息名称和消息
     strcopy(messageNode->messageName, messageName);
     // 消息大小
     messageNode->messageSize = messageSize;
 
+    if(SystemMessageQueue->head != NULL) u_print("SystemMessageQueue->head NULL\n");
     // 查询是否有相同的消息
     MessageNode_t temp = SystemMessageQueue->head;
+
 
     while(temp->next != SystemMessageQueue->tail){
         if(strcmp(temp->next->messageName, messageName) == 0) break;
         temp = temp->next;
     }
+
 
 
     if(strcmp(temp->next->messageName, messageName) == 0){
